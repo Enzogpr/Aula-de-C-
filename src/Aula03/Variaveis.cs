6d94a06a82c21d17bce1db260d1918b2c823368c
Enzo Prado <142319794+Enzogpr@users.noreply.github.com>
2023-10-17 13:20:03 -0300

    aula03

---
+++ src/Aula03/Variaveis.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+
+//class Lesson3{
+
+//    static void Main()
+//    {
+//        byte Num1 = 10; // 0 a 255
+//        int Num = 0; // int = inteiro 
+//        char Letter = 'C';// caractere 
+//        float value = 5.3f; // numero quebrado
+//        string name = "Enzo"; // texto
+
+//        var aux = name; // vai ser atribuido na declaração
+
+//        Console.WriteLine("Valor da variavel:" + aux + "...");
+//    }
+
+//}
+
+using System;
+
+class Lesson3
+{
+
+    static void Main()
+    {
+        int Num1,Num2,Res;
+
+        Num1 = 10;
+        Num2 = 2;
+        Res = Num1 * Num2;
+
+        Console.WriteLine("Valor do resultado é:" + Res + "...");
+    }
+
+}
