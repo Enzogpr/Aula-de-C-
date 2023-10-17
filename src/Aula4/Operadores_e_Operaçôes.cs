ec35db3bec4ef4be6ee15550d7efbee0ec5a57a2
Enzo Prado <142319794+Enzogpr@users.noreply.github.com>
2023-10-17 13:54:14 -0300

    aula04

---
+++ src/Aula4/Operadores_e_Operaçôes.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula4
+{
+     class Operadores_e_Operaçôes
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            bool res = (5>3) | (10<3);   
+                                // & = and / E
+                                // | = Or / ou
+                                // retorna verdadeiro ou falço 
+                                // > maior que // < menor do que // != diferente de // >= maior igua // =< menor igual
+            num++;              //num += 1;    // num = num + 1;
+                                // + * / = 
+            Console.WriteLine(res);
+        }
+    }
+}
