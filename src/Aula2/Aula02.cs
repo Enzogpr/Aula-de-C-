834c55a7a547ecc29afaea315d84759165f98040
Enzo Prado <142319794+Enzogpr@users.noreply.github.com>
2023-10-17 11:11:21 -0300

    aula02 adição

---
+++ src/Aula2/Aula02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// não explicado
+
+namespace Aula2
+{
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CFB Cursos");
+            Console.Write(args.GetValue(0));
+        }
+    }
+}
