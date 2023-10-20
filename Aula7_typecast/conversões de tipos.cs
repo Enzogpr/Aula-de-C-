eaa170a23186d5587ad2feaa64fa2df44a87c828
Enzogpr <enzogonzagap@gmail.com>
2023-10-20 13:51:27 -0300

    aula7

---
+++ Aula7_typecast/conversões de tipos.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula7_typecast
+{
+     class Program
+    {
+        static void Main()
+        {
+           
+            float n1 = 10.5f;
+            int n2 = (int)n1;//type cast
+
+            Console.WriteLine(n2);
+        }
+    }
+}
