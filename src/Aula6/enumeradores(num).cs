80ddad1ec50e05c7cfd2068498960a02b8558e02
Enzogpr <enzogonzagap@gmail.com>
2023-10-20 11:50:49 -0300

    Aula6

---
+++ src/Aula6/enumeradores(num).cs
@@ -0,0 +1,27 @@
+﻿namespace Aula6
+{
+    //     class Program
+    //    {
+
+    //        enum Daysemana { Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado };
+    //        static void Main()
+    //        {
+    //            Daysemana ds = (Daysemana)3;
+
+    //            Console.WriteLine(ds);
+    //        }  
+    //    }
+    //}
+
+    class Program
+    {
+
+        enum Daysemana { Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado };
+        static void Main()
+        {
+            int ds = (int)Daysemana.Sexta;
+
+            Console.WriteLine(ds);
+        }
+    }
+}
