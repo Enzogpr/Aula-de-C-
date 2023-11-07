213b599e7faacd195ba4471465cf88718e96c70a
Enzo Prado <142319794+Enzogpr@users.noreply.github.com>
2023-11-07 14:14:17 -0300

    aula8

---
+++ Aula8_if/if.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula8_if
+{
+    class C_if
+    {
+        static void Main()
+        {
+            int nota;
+            string resultado = "aprovado";
+
+            Console.WriteLine("Digite a nota: ");
+            nota = int.Parse(Console.ReadLine());
+            if (nota >= 60)
+            {
+                Console.WriteLine("------------Resultado-------------\n");
+                Console.WriteLine("            {0}",resultado);
+            } 
+            else
+            {
+                Console.WriteLine("------------Resultado-------------\n");
+                resultado = "Reprovado";
+                Console.WriteLine("              {0}   ",resultado);
+            }
+        }
+    }
+}
