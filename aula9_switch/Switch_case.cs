9be0ac9f94f4e5923c41c01b7856510009a88778
Enzo Prado <142319794+Enzogpr@users.noreply.github.com>
2023-11-07 14:24:47 -0300

    1° parte switch

---
+++ aula9_switch/Switch_case.cs
@@ -0,0 +1,14 @@
+﻿namespace aula9_switch
+{
+     class Switch_case
+    {
+        static void Main()
+        {
+            int tempo = 0, escolha = 0;
+            Console.WriteLine("Belo Horizonte/MG a Vitoria/ES");
+            Console.WriteLine("---------Escola o transporte---------");
+            Console.WriteLine("[1] Avisão  [2]Carro  [3] Onibus");
+
+        }
+    }
+}
